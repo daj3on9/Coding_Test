02efd669d4d0650319f637925a203e6cf2a01777
daj3on9 <dajung5623@naver.com>
2024-10-01 18:39:29 +0900

    [Silver IV] Title: 스택, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,17 +1,18 @@
+import sys
 N = int(input())
 stack = []
 
 for i in range(N):
-  order = input().split()
+  order = sys.stdin.readline().split()
   if order[0] == 'push':
     stack.append(order[1]) # 가장 마지막에 원소 추가
   elif order[0] == 'pop' and stack:
-    print(stack.pop())
+    print(stack.pop()) # 가장 마지막 원소 제거 및 출력
   elif order[0] == 'size':
-    print(len(stack))
+    print(len(stack)) # 배열의 크기 출력
   elif order[0] == 'empty':
-    print(1 if len(stack) == 0 else 0)
+    print(1 if len(stack) == 0 else 0) # 배열 길이가 0이면 1 출력 == 비었다는 뜻!!!
   elif order[0] == 'top' and stack:
-    print(stack[-1])
+    print(stack[-1]) # 배열의 가장 마지막 요소 출력!
   else:
     print(-1)
