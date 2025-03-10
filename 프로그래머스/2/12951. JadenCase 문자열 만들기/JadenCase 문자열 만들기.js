517507cdb311356019ccad8d5f6dd843912871c6
daj3on9 <dajung5623@naver.com>
2025-03-10 17:14:39 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,12 +1,11 @@
 function solution(s) {
-    const arr = s.split(" ");
+    const arr = s.toLowerCase().split(" ");
     
     for (let i in arr) {  
         if (arr[i]) {
-            arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1).toLowerCase();
+            arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1); 
         }
     }
 
     return arr.join(" ");
 }
-
