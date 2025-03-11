41e06d73cf18e371c54bef030231ceef11eed731
daj3on9 <dajung5623@naver.com>
2025-03-11 17:24:32 +0900

    [Bronze V] Title: 나머지, Time: 92 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const [a, b, c] = input[0].split(" ").map(Number);
+
+console.log((a + b) % c);
+console.log(((a % c) + (b % c)) % c);
+console.log((a * b) % c);
+console.log(((a % c) * (b % c)) % c);
