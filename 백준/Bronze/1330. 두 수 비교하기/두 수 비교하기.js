e9853842064bfc8367ba20e9e2a819450103deea
daj3on9 <dajung5623@naver.com>
2025-03-13 16:17:27 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split(" ");
+
+const A = Number(input[0]);
+const B = Number(input[1]);
+
+if (A > B) console.log(`>`);
+else if (A == B) console.log(`==`);
+else console.log(`<`);
