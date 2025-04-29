6c3357477fee0cdd234109841927513e0de7a9c9
daj3on9 <dajung5623@naver.com>
2025-04-29 15:12:57 +0900

    [level 2] Title: 주식가격, Time: 29.88 ms, Memory: 42.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,16 +1,17 @@
 function solution(prices) {
     const answer = [];
     
-    for(let i = 0; i < prices.length ; i++){
-       let count = 0;
+    for(let i = 0; i < prices.length; i++){
         const current = prices[i];
+        let count = 0;
         
-        for(let j = i + 1; j < prices.length; j++){
+        for(let j = i+1; j < prices.length; j++){
             count++;
+            
             if(prices[j] < current) break;
+
         }
-        
-        answer.push(count);
+        answer.push(count)
     }
     return answer
 }
